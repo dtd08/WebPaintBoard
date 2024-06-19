8cbfb6246fee426faa0b2eef346cd60cb437e7b0
dtd08 <126937437+dtd08@users.noreply.github.com>
2024-06-19 14:41:26 +0900

    Update script.js

--- Web_Paint_Board/script.js
+++ Web_Paint_Board/script.js
@@ -1,14 +1,14 @@
-// html에서 canvas 불러오기
+/// html에서 canvas 불러오기
 const canvas = document.getElementById("canvas");  // canvas 저장
 
 
-// context를 선언 및 canvas 기본 셋팅
+/// context를 선언 및 canvas 기본 셋팅
 /*
     캔버스 요소에서 픽셀에 접근할 수 있는 방법 = context를 이용하는 것이다. (context = 캔버스 안의 픽셀들)
     context는 context variable을 선언하여 생성한다.
 */
 const ctx = canvas.getContext("2d"); // 캔버스 안에서 픽셀 컨트롤 설정
-const INITIAL_COLOR = "#2c2c2c";
+const INITIAL_COLOR = "#000";
 
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -32,7 +32,7 @@ function onMouseMove(event) {
     const x = event.offsetX; // 마우스가 캔버스 안에서 움직일 때의 x값
     const y = event.offsetY; //         ''       y값
 
-    // 선 그리기 기능
+    /// 선 그리기 기능
     if (!painting) { // 마우스를 클릭하지 않은 채로 이동할 때
         ctx.beginPath();
         ctx.moveTo(x, y);
@@ -50,7 +50,7 @@ if (canvas) {
 }
 
 
-// 색상 선택 기능
+/// 색상 선택 기능
 const colors = document.getElementsByClassName("color"); // 컬러칩 불러오기
 
 // 컬러칩 배열 내 각 컬러들에 대한 클릭 이벤트 감지
@@ -60,4 +60,31 @@ function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
     ctx.fillStyle = color;
-} 
+}
+
+
+/// 브러쉬 사이즈 조절 기능
+const brush = document.querySelector(".brush"); // 붓 그림  // 클릭 이벤트를 위한 호출
+const brushSize = document.querySelector("#brush-size"); // 슬라이더  // 선 굵기 조정을 위한 호출
+
+// 사이즈 조절 팝업창 표시
+brush.addEventListener("click", (e) => {
+    document.querySelector(".brushSlider").style.display = "block";
+});
+
+brush.addEventListener("dblclick", (e) => {
+    document.querySelector(".brushSlider").style.display = "none";
+});
+
+// 슬라이더 인풋 감지
+if (brushSize) {
+    brushSize.addEventListener("input", handleInput);
+}
+
+// 선 굵기 조정 함수
+function handleInput() {
+    console.log("인풋 감지");
+    let sizeValue = brushSize.value;
+    ctx.lineWidth = sizeValue;
+    console.log(sizeValue);
+}
